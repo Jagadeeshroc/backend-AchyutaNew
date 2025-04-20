651ab18980a505e55b92e53f4fb5f20e2dbff748
jagadeesh <jagadeeshvanganooru@gmail.com>
2025-04-20 21:15:08 +0530

    comments

--- Achyuta/server/server.js
+++ Achyuta/server/server.js
@@ -14,14 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 
-// // Middleware
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL || 'http://localhost:5173/' || "*",
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// }));
-app.options('*', cors());
+// Middleware
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173/' || "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 app.use(express.json());
 
