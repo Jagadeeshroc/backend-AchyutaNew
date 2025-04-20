45e5fa75959457ed241f5d7bbe92741fa3dde1f2
jagadeesh <jagadeeshvanganooru@gmail.com>
2025-04-20 17:15:25 +0530

    removed /

--- Achyuta/server/server.js
+++ Achyuta/server/server.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL||'http://localhost:5173/'||'*',
+    origin: process.env.FRONTEND_URL||'http://localhost:5173'||'*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
