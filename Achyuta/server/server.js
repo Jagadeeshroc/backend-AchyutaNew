18627caf22ca912730aa4e5b02160bc86baa53d6
jagadeesh <jagadeeshvanganooru@gmail.com>
2025-04-20 21:13:00 +0530

    comments

--- Achyuta/server/server.js
+++ Achyuta/server/server.js
@@ -7,29 +7,29 @@ const { initDB } = require('./db');
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
-// const jobRoutes = require('./routes/jobs');
-// const reviewRoutes = require('./routes/reviews');
+const jobRoutes = require('./routes/jobs');
+const reviewRoutes = require('./routes/reviews');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    credentials: true // If you need cookies/authentication
-}));
-
-// Handle preflight requests
+// // Middleware
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:5173/' || "*",
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+// }));
 app.options('*', cors());
+
 app.use(express.json());
 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
-// app.use('/jobs', jobRoutes);
-// app.use('/', reviewRoutes); // reviews are at root because of the jobId path
+app.use('/jobs', jobRoutes);
+app.use('/', reviewRoutes); // reviews are at root because of the jobId path
 
 // Start server
 initDB();
